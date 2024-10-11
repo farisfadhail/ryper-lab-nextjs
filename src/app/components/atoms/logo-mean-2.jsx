3eb964633581593a78d6beb53066763cfc6d1561
Faris Fadhail <farisfadhail@gmail.com>
2024-10-11 16:46:15 +0700

    fix: quote

--- src/app/components/atoms/logo-mean-2.jsx
+++ src/app/components/atoms/logo-mean-2.jsx
@@ -1,11 +1,11 @@
 export default function LogoMean2() {
-  return (
-    <>
-      <div className="flex items-center absolute bottom-16 left-32">
-        <div className="size-3 bg-white rounded-full"></div>
-        <div className="h-1 bg-white w-80"></div>
-      </div>
-      <p className="text-white text-2xl w-1/2 absolute bottom-9 right-36">Burung Belibis yang dalam bahasa Norwegia yaitu 'rype' sebagai dasar nama dari lab kita</p>
-    </>
-  );
+	return (
+		<>
+			<div className="flex items-center absolute bottom-16 left-32">
+				<div className="size-3 bg-white rounded-full"></div>
+				<div className="h-1 bg-white w-80"></div>
+			</div>
+			<p className="text-white text-2xl w-1/2 absolute bottom-9 right-36">Burung Belibis yang dalam bahasa Norwegia yaitu &quot;rype&quot; sebagai dasar nama dari lab kita</p>
+		</>
+	);
 }
